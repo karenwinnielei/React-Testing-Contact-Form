b11c7b38856131db1bdcae7238216d96a325b914
Karen Lei <karenwinnielei@gmail.com>
2020-06-04 16:57:07 -0700

    add screen and async await

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,25 +1,31 @@
 import React from 'react'
-import { render, fireEvent, getAllByTestId } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import ContactForm from './ContactForm'
+import { act } from 'react-dom/test-utils'
 
-test('ContactForm adds new contact info', () => {
-    const { getByLabelText, getByTestId, getByText } = render(<ContactForm />)
+test('ContactForm adds new contact info', async () => {
+    await act(async () => {
+    render(<ContactForm />)
     
-    const firstNameInput = getByLabelText(/first name/i)
-    const lastNameInput = getByLabelText(/last name/i)
-    const emailInput = getByLabelText(/email/i)
-    const messageInput = getByLabelText(/message/i)
+    const firstNameInput = screen.getByLabelText(/first name/i)
+    const lastNameInput = screen.getByLabelText(/last name/i)
+    const emailInput = screen.getByLabelText(/email/i)
+    const messageInput = screen.getByLabelText(/message/i)
 
     fireEvent.change(firstNameInput, { target: { value: 'Karen' }})        
     fireEvent.change(lastNameInput, { target: { value: 'Lei' }})        
     fireEvent.change(emailInput, { target: { value: 'karen@karen.com' }})        
     fireEvent.change(messageInput, { target: { value: 'hello' }})        
 
-    const submitButton = getByTestId(/submit/i)
+    const submitButton = screen.getByTestId(/submit/i)
     fireEvent.click(submitButton)
 
     expect(firstNameInput.value).toBe('Karen')
     expect(lastNameInput.value).toBe('Lei')
     expect(emailInput.value).toBe('karen@karen.com')
     expect(messageInput.value).toBe('hello')
+
+    // const newContact = screen.getByText(/karen/i);
+
+})
 })
